5283d42e75f50bd8ef2f50cf6cf4f8e1fcb0c4e2
hmtheboy154 <buingoc67@gmail.com>
2024-09-22 14:00:38 -0400

    Utils: Adding esdfs to IsSdcardfsUsed()
    Supporting esdfs as an alternative to sdcardfs
    
    Change-Id: Ic2536f409a6de44b3c9d17bf6a138ac576ea4eb1
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- Utils.cpp
+++ Utils.cpp
@@ -1123,7 +1123,7 @@ bool IsFilesystemSupported(const std::string& fsType) {
 }
 
 bool IsSdcardfsUsed() {
-    return IsFilesystemSupported("sdcardfs") &&
+    return (IsFilesystemSupported("sdcardfs") || IsFilesystemSupported("esdfs")) &&
            base::GetBoolProperty(kExternalStorageSdcardfs, true);
 }
 
